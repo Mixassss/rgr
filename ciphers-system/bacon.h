e4a4f29f83a92cdec510ee18f231c6ca94028a68
Mixassss <163171930+Mixassss@users.noreply.github.com>
2024-10-19 22:43:13 +0700

    Добавление хеддера bacon

---
+++ ciphers-system/bacon.h
@@ -0,0 +1,13 @@
+#ifndef BACON_H
+#define BACON_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<char> checkinputbacon(const string& message);
+string baconEncryption(const string& message);
+string baconDecryption(const string& message);
+
+#endif // BACON_H
