f74bdc8a2ee1f0c61a87c9fa5bf302b2a5a09d65
Mixassss <bigboommaximandsim@gmail.com>
2024-10-20 22:16:26 +0700

    хеддер a1z26

--- ciphers-system/a1z26.h
+++ ciphers-system/a1z26.h
@@ -6,9 +6,12 @@
 
 using namespace std;
 
+bool isLetter(char stroka);
+char toUpper(char stroka);
 vector<char> checkinputa1z26(string message);
 string a1z26Encryption(string message, string key);
 string a1z26Decryption(string message, string key);
 string generateCipherAlphabet(string key);
+string myToString(int number);
 
 #endif // A1Z26_H
